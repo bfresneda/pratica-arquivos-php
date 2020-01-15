7cdb365c90aa9f71622dae69caf6210865b83505
Bruno Fresneda <bruno.fresneda@gmail.com>
2020-01-15 12:16:59 -0300

    alteracoes

--- atividade.php
+++ atividade.php
@@ -2,11 +2,12 @@
 //gravando o conteudo do GET na variavel de $texto
 if($_GET){
     $texto = $_GET['texto'];
-    
+    $texto = $texto . "\n";
     $arquivo = fopen('mensagemdeteste.txt','a'); //pode ser utilizado outros modos
     //escrevendo no arquivo $arquivo as informações recebidas pelo GET $texto
+
       fwrite($arquivo,$texto);
-    
+      
       fclose($arquivo);
     
       echo 'informação salva';
